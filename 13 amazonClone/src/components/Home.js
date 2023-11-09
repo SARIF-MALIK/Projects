02081a77a6d0d8d9494ccb448b51021ad4a3d9c2
SARIF <sarif.malik@hotmail.com>
2023-11-09 18:16:40 +0530

    node js basic cmd

--- 13 amazonClone/src/components/Home.js
+++ 13 amazonClone/src/components/Home.js
@@ -1,43 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
-import NavBarLanding from './NavBarLanding'
+import MainContent from './MainContent'
+import Stats from './Stats'
 const Home = () => {
   return (
     <>
         <Navbar/>
-        <NavBarLanding/>
-        <section classNameName="container">
-        <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
-            <div classNameName="carousel-inner">
-              <div classNameName="carousel-item active">
-                <img src="./media/crousal1.jpg" classNameName="d-block w-100" alt="..."/>
-              </div>
-              <div classNameName="carousel-item">
-                <img src="./media/crousal2.jpg" className="d-block w-100" alt="..."/>
-              </div>
-              <div className="carousel-item">
-                <img src="./media/crousal3.jpg" className="d-block w-100" alt="..."/>
-              </div>
-              <div className="carousel-item">
-                <img src="./media/crousal4.jpg" className="d-block w-100" alt="..."/>
-              </div>
-              <div className="carousel-item">
-                <img src="./media/crousal5.jpg" className="d-block w-100" alt="..."/>
-              </div>
-              <div className="carousel-item">
-                <img src="./media/crousal6.jpg" className="d-block w-100" alt="..."/>
-              </div>
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          </div>
-          </section>
+        {/* <MainContent/> */}
+        <Stats></Stats>
     </>
   )
 }
---
+++ 13 amazonClone/src/components/MainContent.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function mainContent() {
+  return (
+           <section classNameName="container">
+    <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
+        <div classNameName="carousel-inner">
+          <div classNameName="carousel-item active">
+            <img src="./media/crousal1.jpg" classNameName="d-block w-100" alt="..."/>
+          </div>
+          <div classNameName="carousel-item">
+            <img src="./media/crousal2.jpg" className="d-block w-100" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <img src="./media/crousal3.jpg" className="d-block w-100" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <img src="./media/crousal4.jpg" className="d-block w-100" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <img src="./media/crousal5.jpg" className="d-block w-100" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <img src="./media/crousal6.jpg" className="d-block w-100" alt="..."/>
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+      </section>
+  )
+}
+
+export default mainContent
