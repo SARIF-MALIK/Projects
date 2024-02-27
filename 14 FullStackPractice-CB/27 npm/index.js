3bf658b79c44a13133c61bb8cef212e56258a4ce
SARIF <sarif.malik@hotmail.com>
2024-02-27 11:23:10 +0530

    Your commit message

---
+++ 14 FullStackPractice-CB/27 npm/index.js
@@ -0,0 +1,46 @@
+
+// // const giveMeAJoke = require('give-me-a-joke');
+
+// // giveMeAJoke.getRandomDadJoke (function(joke) {
+// //     console.log(joke);
+// // });
+
+
+// const figlet = require("figlet");
+// const Color = require('colors');
+
+// // figlet("Welcome to BackEnd!!", function (err, data) {
+// //   if (err) {
+// //     console.log("Something went wrong...");
+// //     console.dir(err);
+// //     return;
+// //   }
+// //   console.log(data);
+// // });
+
+// figlet.text(
+//     "Welcome to Backend",
+//     {
+//       font: "Ghost",
+//       horizontalLayout: "default",
+//       verticalLayout: "default",
+//       width: 80,
+//       whitespaceBreak: true,
+//     },
+//     function (err, data) {
+//       if (err) {
+//         console.log("Something went wrong...");
+//         console.dir(err);
+//         return;
+//       }
+//       console.log(data.rainbow);
+//     }
+//   );
+
+let path = require('path')
+
+let joinedPath = path.join('hey///', '\siri', 'callSuresh'); 
+console.log(joinedPath); 
+
+
+
