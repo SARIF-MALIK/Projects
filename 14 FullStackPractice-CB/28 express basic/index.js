6a1e311fea5a4059175647a27ecf82782bf47a89
SARIF <sarif.malik@hotmail.com>
2024-02-28 11:23:34 +0530

    Your commit message

---
+++ 14 FullStackPractice-CB/28 express basic/index.js
@@ -0,0 +1,14 @@
+const express = require('express');        /// returns a function 
+
+const app = express();              /// return object -> entire instance of your application 
+
+
+app.listen(8080, ()=>{
+    console.log('server connected!!!')
+})
+
+app.get('/', (req, res)=>{
+    res.send('hello')
+})
+
+
